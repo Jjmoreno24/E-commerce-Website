ea457ee02408ecdc62a8b1631087f93e25d03c61
Jjmoreno24 <jm2462001@gmail.com>
2025-08-21 08:17:41 -0500

    ruta de productos DB js

---
+++ backend/routes/productosRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Producto = require('../model/Producto');
+
+// Obtener todos los productos
+router.get('/', async (req, res) => {
+  try {
+    const productos = await Producto.find();
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ mensaje: 'Error al obtener productos' });
+  }
+});
+
+// Obtener un producto por ID
+router.get('/:id', async (req, res) => {
+  try {
+    const producto = await Producto.findById(req.params.id);
+    if (!producto) return res.status(404).json({ mensaje: 'Producto no encontrado' });
+    res.json(producto);
+  } catch (error) {
+    res.status(500).json({ mensaje: 'Error al obtener el producto' });
+  }
+});
+
+module.exports = router;
